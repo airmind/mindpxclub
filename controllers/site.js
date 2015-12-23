67a9b7267af863171a58e3212d85e4c1e2fcc804
Henry Zhang <zhanghui629@gmail.com>
2015-12-23 10:56:15 +0800

    Mobile app download for mindclub.

--- controllers/site.js
+++ controllers/site.js
@@ -144,8 +144,8 @@ exports.sitemap = function (req, res, next) {
 
 exports.appDownload = function (req, res, next) {
   if (/Android/i.test(req.headers['user-agent'])) {
-    res.redirect('http://fir.im/ks4u');
+    res.redirect("/public/mobile_app/MindClub.apk");
   } else {
-    res.redirect('https://itunes.apple.com/cn/app/id954734793');
+    res.redirect('https://itunes.apple.com/us/app/mind-club/id1039897749?ls=1&mt=8');
   }
 };
